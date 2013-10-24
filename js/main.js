17b109c4c38a6d6407251149d34458decc69304d
sharkinsspatial <sharkinsgis@gmail.com>
2013-10-24 11:03:52 -0500

    Updated MapBox API to v 1.4.0

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 var map = L.mapbox.map('map', 'sharkins.map-uwias8cf');
 var geojson = {type:'FeatureCollection', features:[]};
-$.ajax('http://api.geonames.org/rssToGeoRSS?feedUrl=https%3A%2F%2Fnews.google.com%2Fnews%2Ffeeds%3Fgl%3Dca%26q%3Dperu%2Bmining%2Bprotests%26um%3D1%26ie%3DUTF-8%26output%3Drss%26num%3D30&username=sharkinsgis&feedLanguage=en&country=pe').done(function(xml) {
+var geonamesUrl = 'http://api.geonames.org/rssToGeoRSS?feedUrl=https%3A%2F%2Fnews.google.com%2Fnews%2Ffeeds%3Fgl%3Dca%26q%3Dperu%2Bmining%2Bprotests%26um%3D1%26ie%3DUTF-8%26output%3Drss%26num%3D30&username=sharkinsgis&feedLanguage=en&country=pe&addUngeocodedItems=false';
+$.ajax(geonamesUrl).done(function(xml) {
     $(xml).find('item').each(function(){
-       //console.log($(this).children('title').text());
-       createFeature(this);
+       if ($(this).children('geo\\:long').length > 0){
+           createFeature(this);
+       }
    });
 console.log(geojson);   
+L.geoJson(geojson).addTo(map);
 });
 function createFeature(xmlitem){
-    var feature = {type: 'Feature', properties:{}};
-    console.log($(xmlitem).children('title').text());
+    var feature = {type: 'Feature', properties:{}, geometry:{type: 'Point'}};
     feature.properties.title = $(xmlitem).children('title').text();
-    coords = [$(xmlitem).children('geo\\:long'),$(xmlitem).children('geo\\:lat')];
-    feature.properties.coordinates = coords;  
+    coords = [parseFloat($(xmlitem).children('geo\\:long').text()),
+                         parseFloat($(xmlitem).children('geo\\:lat').text())];
+    feature.geometry.coordinates = coords;  
     geojson.features.push(feature);
 } 
