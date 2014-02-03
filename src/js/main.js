8dd9d464521557659b53b2c7e5aa4ea4aa1e7d32
sharkinsspatial <sharkinsgis@gmail.com>
2014-02-03 12:59:55 -0500

    Cleaned up RSS Urls

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
 var map = L.mapbox.map('map', 'sharkins.map-uwias8cf',{maxZoom:12});
-var geonamesUrl = 'http://api.geonames.org/rssToGeoRSS?feedUrl=https%3A%2F%2Fnews.google.com%2Fnews%2Ffeeds%3Fgl%3Dca%26q%3Dperu%2Bmining%2Bprotests%26um%3D1%26ie%3DUTF-8%26output%3Drss%26num%3D30&username=sharkinsgis&feedLanguage=en&country=pe&addUngeocodedItems=false';
 var markerMap = {};
 var markersList = [];
-$.ajax(geonamesUrl).done(function(xml) {
+$.ajax(buildRSSUrl()).done(function(xml) {
     $(xml).find('item').each(function(index){
-       if ($(this).children('geo\\:long').length > 0){
+        if ($(this).children('geo\\:long').length > 0){
            createFeature(index,this);
            addListItem(index, this);
        }
     });
-    
     $('li').sort(function(a,b) {
         return new Date($(a).attr('datetime')) - new Date($(b).attr('datetime'));
     }).each(function() {
@@ -36,14 +34,43 @@ $.ajax(geonamesUrl).done(function(xml) {
     markers.addLayers(markersList);
     map.addLayer(markers);
 });
-function createFeature(index,xmlitem){
+function buildRSSUrl() {
+    var googleUrl = 'https://news.google.com/news/feeds';
+    var googleParams = {
+        gl: 'ca',
+        q: 'peru+mining+protests',
+        um: 1,
+        ie: 'UTF-8',
+        output: 'rss',
+        num: 200
+    }; 
+    var encodedGoogleUrl = encodeURIComponent(googleUrl + '?' + 
+                                   buildQueryString(googleParams));
+    var geonamesUrl = 'http://api.geonames.org/rssToGeoRSS';
+    var geonamesParams = {
+        feedUrl: encodedGoogleUrl,
+        username: 'sharkinsgis',
+        feedLanguage: 'en',
+        country: 'pe',
+        addUngeocodedItems: 'false'
+    };
+    geonamesUrl = geonamesUrl + '?' + buildQueryString(geonamesParams);
+    return geonamesUrl;
+}
+function buildQueryString(data) {
+    var params = [];
+    for (var d in data)
+        params.push(d + '=' + data[d]);
+    return params.join('&');
+}
+function createFeature(index,xmlitem) {
     var latlng = new L.LatLng(parseFloat($(xmlitem).children('geo\\:lat').text()),
                          parseFloat($(xmlitem).children('geo\\:long').text())); 
     var marker = L.marker(latlng);
     markersList.push(marker);
     markerMap[index] = marker;
 }
-function addListItem(index, xmlitem){
+function addListItem(index, xmlitem) {
     var articleTitle = $(xmlitem).children('title').text(); 
     var pubdate = new Date($(xmlitem).children('pubDate').text());
     var newListItem = $('<li/>', {
