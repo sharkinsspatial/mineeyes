8030c72798c00fde938a7c64b0c10e8b94766e2e
sharkinsspatial <sharkinsgis@gmail.com>
2014-02-07 12:45:41 -0500

    Consolidated jQuery selectors

--- src/js/main.js
+++ src/js/main.js
@@ -13,41 +13,44 @@ var app = (function ($,L) {
             spiderfyDistanceMultiplier:1, 
             showCoverageOnHover:false
         });
-
+        var articleList = $('#articlelist');
         $.ajax(buildRSSUrl()).done(function(xml) {
             $(xml).find('item').each(function(index){
-                var $xmlitem = $(this);
-                if ($xmlitem.children('geo\\:long').length > 0){
-                    createFeature(index, $xmlitem);
-                    addListItem(index, $xmlitem);
+                var xmlitem = $(this);
+                if (xmlitem.children('geo\\:long').length > 0){
+                    createFeature(index, xmlitem);
+                    addListItem(index, xmlitem, articleList);
                 }
             });
-            sortByDate($('li'),$('#articlelist'));
-            $('#articlelist li').on('click', articleClick);
+            var listItems = $('#articlelist li');
+            sortByDate(listItems, articleList);
+            listItems.on('click', articleClick);
             
             _markers.addLayers(_markersList);
             _map.addLayer(_markers);
         });
     }
     function articleClick(e) {
-        previousActiveMarker = _markerMap[$('li.active').attr('id')];  
+        var previousActiveLi = $('li.active');
+        previousActiveMarker = _markerMap[previousActiveLi.attr('id')];  
         if (previousActiveMarker) {
             previousActiveMarker.setIcon(new L.Icon.Default());
         }
-        $('li.active').removeClass('active');
-        $(this).addClass('active');
-        activeMarker = _markerMap[$(this).attr('id')];
+        previousActiveLi.removeClass('active');
+        var activeLi = $(this);
+        activeLi.addClass('active');
+        activeMarker = _markerMap[activeLi.attr('id')];
         _markers.zoomToShowLayer(activeMarker, function(){
             activeMarker.setIcon(_activeIcon);
         });    
     }
-    function sortByDate(list,div) {
-        if(list.length && div.length) {
-            list.sort(function(a,b) {
+    function sortByDate(listItems, list) {
+        if(listItems.length && list.length) {
+            listItems.sort(function(a,b) {
                 return new Date($(a).attr('datetime')) - 
                     new Date($(b).attr('datetime'));
             }).each(function() {
-                div.prepend(this);
+                list.prepend(this);
             }); 
         }
         else {
@@ -84,24 +87,24 @@ var app = (function ($,L) {
         geonamesUrl = geonamesUrl + '?' + buildQueryString(geonamesParams);
         return geonamesUrl;
     }
-    function createFeature(index,$xmlitem) {
-        var latlng = new L.LatLng(parseFloat($xmlitem
+    function createFeature(index, xmlitem) {
+        var latlng = new L.LatLng(parseFloat(xmlitem
                                              .children('geo\\:lat').text()),
-                                  parseFloat($xmlitem
+                                  parseFloat(xmlitem
                                              .children('geo\\:long').text())); 
                                   var marker = L.marker(latlng);
                                   _markersList.push(marker);
                                   _markerMap[index] = marker;
     }
-    function addListItem(index, $xmlitem) {
-        var articleTitle = $xmlitem.children('title').text(); 
-        var pubdate = new Date($xmlitem.children('pubDate').text());
+    function addListItem(index, xmlitem, articleList) {
+        var articleTitle = xmlitem.children('title').text(); 
+        var pubdate = new Date(xmlitem.children('pubDate').text());
         var newListItem = $('<li/>', {
             html: articleTitle,
             'id': index,
             'datetime': pubdate  
         });
-        $('#articlelist').append(newListItem);
+        articleList.append(newListItem);
     }
     return {
         init: init,
