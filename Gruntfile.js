8ba417e9066665b9c1fcb767a653036ea86a73c6
sharkinsspatial <sharkinsgis@gmail.com>
2014-02-25 13:36:17 -0500

    Fixed dependencies for PhantomJS and MiniMap

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,14 @@
 module.exports = function(grunt) {
     //Project configuration.
-    grunt.initConfig({
+    grunt.initConfig({        
         jasmine : {
             src : 'src/**/main.js',
             options : {
                 specs : 'spec/**/*.js',
-                vendor: ['src/js/vendor/*.js']
+                vendor: ['src/js/vendor/mapbox.js',
+                    'src/js/vendor/leaflet.markercluster.js',
+                    'src/js/vendor/jquery-1.11.0.min.js',
+                    'src/js/vendor/Control.MiniMap.js']
             }
         }
     });
