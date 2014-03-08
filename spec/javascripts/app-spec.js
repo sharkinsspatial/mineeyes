80d7a7f8c24b44ff5c09038708bfd37ed63d1b87
sharkinsspatial <sharkinsgis@gmail.com>
2014-03-08 17:37:38 -0500

    Fixed tests

--- spec/javascripts/app-spec.js
+++ spec/javascripts/app-spec.js
@@ -1,44 +1,33 @@
-describe('sortByDate', function() {
+describe('app.articleList.sortByDate', function() {
     beforeEach(function() {
-        setFixtures('<ul id="articlelist"></ul>');
+        setFixtures('<div id="articles"></div>');
+    });
+    it('should use DOM fixture', function() {
+        expect($('#articles')).toHaveLength(1);
+    });
+    it('should place newest li item on top', function() {
+        app.articleList.init();  
         var pubdateFirst = new Date(2014, 1, 1, 0, 0, 0);
         var pubdateSecond = new Date(2014, 1, 2, 0, 0, 0);
         $('<li/>',{'datetime': pubdateFirst}).appendTo('#articlelist'); 
         $('<li/>',{'datetime': pubdateSecond}).appendTo('#articlelist'); 
-    });
-    it('should use DOM fixture', function() {
-        expect($('li')).toHaveLength(2);
-    });
-    it('should throw TypeError with undefined elements', function() {
-        expect(app.sortByDate.bind(null, null)).toThrowError(TypeError);
-    });
-    it('should throw Error with empty jQuery selections', function() {
-        expect(app.sortByDate.bind($(''), $(''))).toThrow();
-    });
-    it('should place newest li item on top', function() {
-        app.sortByDate($('li'), $('#articlelist'));
+        app.articleList.sortByDate();
         expect($('#articlelist li:nth-child(1)').attr('datetime'))
                .toBeGreaterThan($('#articlelist li:nth-child(2)')
                                   .attr('datetime'));
     });
 });
-describe('createFeature', function() {
-    var markerList;
-    var markerMap;
+
+describe('app.articleMarkers.addMarker', function() {
     var xmlItem;
     beforeEach(function() {
-        markerList = [];
-        markerMap = {};
         var xml = '<item><geo:lat xmlns:geo="test">5.1</geo:lat>' + 
             '<geo:long xmlns:geo="test">1.4</geo:long></item>';
         var xmlDocument = $.parseXML(xml);
         xmlItem = $(xmlDocument).find('item');
     });
-    it('should add marker to markerList array', function() { 
-        app.createFeature(0, xmlItem, markerList, markerMap);
-        expect(markerList.length).toEqual(1);
-    });
-    it('should throw error with null xmlItem', function() {
-        expect(app.createFeature.bind(0, null)).toThrow();
+    it('should add marker to markerList', function() { 
+        app.articleMarkers.addMarker(0, xmlItem);
+        expect(app.articleMarkers.getMarkerList().length).toEqual(1);
     });
 });
